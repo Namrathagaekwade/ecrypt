18c23f63ea1d981239e1df9fa8c8f76b55bbb00d
Namrathagaekwade <60082736+Namrathagaekwade@users.noreply.github.com>
2021-01-17 13:07:45 +0100

    Update ecrypt.py

--- ecrypt.py
+++ ecrypt.py
@@ -13,46 +13,46 @@ download_dir = os.path.join(app.instance_path, 'downloads')
 def upload_file():
    return render_template('upload.html')
 
-@app.route('/encrypt', methods = ['POST'])
-def uploadenc():
-   bufferSize = 64 * 1024
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(os.path.join(uploads_dir,f.filename))
-      upload_file = os.path.join(uploads_dir, f.filename)       
-      password = request.form['text']
-      encfile = f.filename + ".aes"
-      download_file = os.path.join(download_dir, encfile)
-      pyAesCrypt.encryptFile(upload_file, download_file , password , bufferSize) 
-      @after_this_request
-      def remove_enc(response):
-         os.remove(upload_file)
-         os.remove(download_file)
-         return response
-      return send_file(download_file, as_attachment=True) 
+# @app.route('/encrypt', methods = ['POST'])
+# def uploadenc():
+#    bufferSize = 64 * 1024
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       f.save(os.path.join(uploads_dir,f.filename))
+#       upload_file = os.path.join(uploads_dir, f.filename)       
+#       password = request.form['text']
+#       encfile = f.filename + ".aes"
+#       download_file = os.path.join(download_dir, encfile)
+#       pyAesCrypt.encryptFile(upload_file, download_file , password , bufferSize) 
+#       @after_this_request
+#       def remove_enc(response):
+#          os.remove(upload_file)
+#          os.remove(download_file)
+#          return response
+#       return send_file(download_file, as_attachment=True) 
 
-@app.route('/reload', methods = ['POST'])
-def reload():
-   if request.method == 'POST':
-      return render_template('upload.html') 
+# @app.route('/reload', methods = ['POST'])
+# def reload():
+#    if request.method == 'POST':
+#       return render_template('upload.html') 
 
-@app.route('/decrypt', methods = ['POST'])
-def uploaddec():
-   bufferSize = 64 * 1024
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(os.path.join(uploads_dir,f.filename))
-      upload_file = os.path.join(uploads_dir, f.filename)        
-      password = request.form['text']
-      decfile = f.filename.split('.aes')
-      download_file = os.path.join(download_dir,  decfile[0])
-      pyAesCrypt.decryptFile(upload_file, download_file , password , bufferSize)  
-      @after_this_request
-      def remove_dec(response):
-         os.remove(upload_file)
-         os.remove(download_file)
-         return response
-      return send_file(download_file, as_attachment=True) 
+# @app.route('/decrypt', methods = ['POST'])
+# def uploaddec():
+#    bufferSize = 64 * 1024
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       f.save(os.path.join(uploads_dir,f.filename))
+#       upload_file = os.path.join(uploads_dir, f.filename)        
+#       password = request.form['text']
+#       decfile = f.filename.split('.aes')
+#       download_file = os.path.join(download_dir,  decfile[0])
+#       pyAesCrypt.decryptFile(upload_file, download_file , password , bufferSize)  
+#       @after_this_request
+#       def remove_dec(response):
+#          os.remove(upload_file)
+#          os.remove(download_file)
+#          return response
+#       return send_file(download_file, as_attachment=True) 
 
 
 if __name__ == '__main__':
